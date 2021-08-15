17870379ca6c579762a9d9b1b5ae1b725aed32ff
younghyo_desktop <yhchoi0102@gmail.com>
2021-08-15 18:27:33 +0900

    [part3] add views

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+# Django 프로젝트 내에 앱이 여러 개 있을 때, Django가 각 앱들의 url을 구분하기 위해 namespace 지정
+app_name = "polls"
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
